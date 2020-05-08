cb57fd21110ae08d8f7aee0c7369585421e256fb
Hogwarts <18368160864@163.com>
2020-05-08 16:24:20 +0800

    2020-05-08 16:24

---
+++ Practice/calc.py
@@ -0,0 +1,27 @@
+# -*- coding:utf-8 -*-
+# @Time : 2020/5/8 16:12 
+# @Author : Zhang Jingyao
+# @File : calc.py 
+# @Software: PyCharm
+
+class Calc:
+
+    # 加法
+    def addition(self, x, y):
+        z = x + y
+        return z
+
+    # 减法
+    def substarction(self, x, y):
+        z = x - y
+        return z
+
+    # 乘法
+    def multiplication(self, x, y):
+        z = x * y
+        return z
+
+    # 除法
+    def division(self, x, y):
+        z = x / y
+        return z
--- Practice/practice_01.py
+++ Practice/practice_01.py
@@ -4,39 +4,4 @@
 # @File : practice_01.py 
 # @Software: PyCharm
 
-import sys
-import os
-
-# 猜数字游戏
-
-# target_num = int(input("Please input target number:"))
-# input_num = int(input("Please input a number:"))
-#
-# while target_num != input_num:
-#     if target_num > input_num:
-#         input_num = int(input("The number is little small, please input again:"))
-#     elif target_num < input_num:
-#         input_num = int(input("The number is little big, please input again:"))
-# else:
-#     print("Congratulations!! You got it!!")
-
-#
-# print(__file__)
-# print(os.path.abspath(__file__))
-# print(os.path.dirname(os.path.abspath(__file__)))
-# print(os.path.dirname(__file__))
-# print(os.path.join(os.path.dirname(__file__),'txt.txt'))
-
-# file_dir = os.path.join(os.path.dirname(__file__), 'txt.txt')
-# with open(file_dir, 'r', encoding='utf-8') as f:
-#     read_data = f.read()
-#     f.close()
-# print(read_data)
-
-while True:
-    try:
-        x = int(input("Please enter a number: "))
-        break
-    except ValueError:
-        print("Oops!  That was no valid number.  Try again...0000000000")
-
+import pytest
